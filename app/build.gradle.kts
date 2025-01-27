bfd9a381bc8d841da36488e73e513fc3181ee05a
vip66 <vip66@PVSTUDIOS>
2025-01-27 10:46:01 +0300

    Session -2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,13 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(platform("io.github.jan-tennert.supabase:bom:3.0.3"))
+    implementation("io.github.jan-tennert.supabase:postgrest-kt")
+    implementation("io.github.jan-tennert.supabase:auth-kt")
+    implementation("io.github.jan-tennert.supabase:realtime-kt")
+
+    implementation("io.ktor:ktor-client-cio:3.0.0-rc-1")
+
+    implementation("io.github.jan-tennert.supabase:serializer-jackson:2.18.2")
 }
